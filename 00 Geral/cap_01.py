77365487ce9f7370d245a915da97cf54d36d60d2
gabyalexandrino <gabyzinha@gmail.com>
2023-02-07 16:51:05 -0300

    Estudando listas dia 07/02/23

---
+++ 00 Geral/cap_01.py
@@ -0,0 +1,44 @@
+#mesage = "Hello Python world!"
+#print(mesage)
+#mesage = "Hello Python Crash Course world!"
+#print(mesage)
+
+#name = "Ada Lovelace"
+#print(name.upper())
+#print(name.lower())
+
+#first_name = "ada"
+#last_name = "lovelace"
+#full_name = first_name + " " + last_name
+#print(full_name)
+
+#first_name = "ada"
+#last_name = "lovelace"
+#full_name = first_name + " " + last_name
+#message = "Hello, " + full_name.title() + "!"
+
+#print("\tPython")
+#print("Languages:\n\tPython\n\tC\n\tJavaScript")
+
+#favorite_language = 'python '
+#print(favorite_language)
+#favorite_language.rstrip() 
+#print(favorite_language)
+
+#favorite_language = 'python '
+#favorite_language = favorite_language.rstrip()
+#print(favorite_language)
+
+#favorite_language = ' python '
+#print(favorite_language.rstrip())
+#print(favorite_language.lstrip())
+#print(favorite_language.strip()) 
+
+#message = "One of Python's strengths is its diverse community."
+#print(message)
+
+#age = 23
+#message = "Happy " + str(age) + "rd Birthday!"
+
+#import this
+
---
+++ 03 Introducao as listas/3-04_guest_list.py
@@ -0,0 +1,6 @@
+guest_list = ['margarida', 'fatima', 'marilia', 'gilvan']
+
+print("Por favor, venha jantar " + guest_list[0].title() + ".")
+print("Por favor, venha jantar " + guest_list[1].title() + ".")
+print("Por favor, venha jantar " + guest_list[2].title() + ".")
+print("Por favor, venha jantar " + guest_list[3].title() + ".")
