69ef94eb3fea2af8fe24edd294ece50a847a49bd
gabyalexandrino <gabyzinha@gmail.com>
2023-02-13 16:52:12 -0300

    estudo dia 13/02/23

--- 00 Geral/cap_04.py
+++ 00 Geral/cap_04.py
@@ -45,13 +45,58 @@
 #print(max(digits))
 #print(sum(digits))
 
-players = ['charles', 'martina', 'michael', 'florence', 'eli']
-print(players[0:3])
+#players = ['charles', 'martina', 'michael', 'florence', 'eli']
+#print(players[0:3])
+#print(players[1:4])
+#print(players[:4])
+#print(players[2:])
+#print(players[-3:])
 
-print(players[1:4])
+#players = ['charles', 'martina', 'michael', 'florence', 'eli']
+#print("Here are the first three players on my team:")
+#for player in players[:3]:
+#    print(player.title())
 
-print(players[:4])
+#my_foods = ['pizza', 'falafel', 'carrot cake']
+#friend_foods = my_foods[:]
+#print("My favorite foods are:")
+#print(my_foods)
+#print("\nMy friend's favorite foods are:")
+#print(friend_foods)
+#my_foods.append('cannoli')
+#friend_foods.append('ice cream')
+#print("\nMy favorite foods are:")
+#print(my_foods)
+#print("\nMy friend's favorite foods are:")
+#print(friend_foods)
 
-print(players[2:])
+#! This doesn't work(as duas variáveis apontam para a mesma lista):
+#my_foods = ['pizza', 'falafel', 'carrot cake']
+#friend_foods = my_foods
+#print("My favorite foods are:")
+#print(my_foods)
+#print("\nMy friend's favorite foods are:")
+#print(friend_foods)
+#my_foods.append('cannoli')
+#friend_foods.append('ice cream')
+#print("\nMy favorite foods are:")
+#print(my_foods)
+#print("\nMy friend's favorite foods are:")
+#print(friend_foods)
 
-print(players[-3:])
+#dimensions = (200,50)
+#print(dimensions[0])
+#print(dimensions[1])
+
+#dimensions = (200,50)
+#for dimension in dimensions:
+#    print(dimension)
+
+#dimensions = (200, 50)
+#print("Original dimensions:")
+#for dimension in dimensions:
+#    print(dimension)
+#dimensions = (400, 100)
+#print("\nModified dimensions:")
+#for dimension in dimensions:
+#    print(dimension)    
